77c1dbe76c680061713c5df67d66141aa7918bc5
pdf114514 <haruton321@outlook.jp>
2023-10-20 01:37:32 +0900

    Commit

--- Lobbybot.Shared/LobbybotConfig.cs
+++ Lobbybot.Shared/LobbybotConfig.cs
@@ -2,9 +2,18 @@ namespace Lobbybot.Shared;
 
 public class LobbybotConfig {
     public WebConfig Web { get; set; } = new();
+    public Dictionary<string, BotConfig> Bots { get; set; } = new(); // AccountId: <BotConfig>
 }
 
 public class WebConfig {
     public bool PasswordEnabled { get; set; } = false;
     public string Password { get; set; } = "password";
+}
+
+public class BotConfig {
+    public bool AutoStart { get; set; } = false;
+    public string Outfit { get; set; } = "CID_001_Athena_Commando_F_Default";
+    public string Backpack { get; set; } = "";
+    public string Pickaxe { get; set; } = "DefaultPickaxe";
+    public string Emote { get; set; } = "EID_DanceMoves";
 }
